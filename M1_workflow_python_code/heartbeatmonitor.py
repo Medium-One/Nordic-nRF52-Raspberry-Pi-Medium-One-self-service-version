364cd9ff40b16fd806154440514bc1650017fbe5
Michael Li <mikelisfbay@yahoo.com>
2018-07-16 12:19:18 -0700

    Update heartbeatmonitor.py

--- M1_workflow_python_code/heartbeatmonitor.py
+++ M1_workflow_python_code/heartbeatmonitor.py
@@ -1,5 +1,5 @@
-﻿'''
-This workflows monitors the acceleration data and detects motion.  If the device shakes past the impact threshold,
+'''
+This workflows monitors the heart rate.  If the heart rate exceeds the threshold level,
 we send an email alert and SMS alert to the email and phone number listed. It will send maximum one alert 
 per day.
 '''
@@ -12,16 +12,16 @@ email_address = "youracct@site"
  
 alert_message = 'Heads up!  Your heart beat exceeds the threshold level!'
  
-impact_threshold =  210
+heartrate_threshold =  210
  
 heartrate= IONode.get_input('in1')['event_data']['value']
  
-# This checks if there was impact, and if it has been more than 24 hours since sending 
+# This checks if the heart rate exceeds the threshold level and if it has been more than 24 hours since sending 
 # an alert for this device.
-if (heartrate > impact_threshold) and not Store.get('sent_alert'):
+if (heartrate > heartrate_threshold) and not Store.get('sent_alert'):
     log("detected")
     email = Email.Email(sender='alerts@medium.one', display_name='Medium One Alerts',
-                recipients=[email_address], subject='Alert: Motion Detected', message=alert_message, attachments=None)
+                recipients=[email_address], subject='Alert: Exceed Heart Rate Threshold', message=alert_message, attachments=None)
     email.send()
     FreeSMS.sendSMS(phone_number, alert_message) 
     Store.set_data('sent_alert', 'true', ttl=86400) # 86400 seconds = 1 day
